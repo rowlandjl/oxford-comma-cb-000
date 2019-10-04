0e4d9aaebdd36e3a4e6f1e571e6da21e1be3851f
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 01:36:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length == 1
-    array[0]
+    array.first
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
     array[-2] = "and"
-    array
+
   end
 end
